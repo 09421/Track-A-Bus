05ea6df3dfc6a3f70028ff48dca505d4be600840
09421 <lasse@micorbis.dk>
2013-12-03 09:49:25 +0100

    Removed some files

---
+++ 5 - Software/Hjemmeside/MapDrawRouteTool/MapDrawRouteTool/Models/Waypoint.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MapDrawRouteTool.Models
+{
+    public class Waypoint
+    {
+        public int ID;
+        public decimal Lat;
+        public decimal Lng;
+    }
+}
