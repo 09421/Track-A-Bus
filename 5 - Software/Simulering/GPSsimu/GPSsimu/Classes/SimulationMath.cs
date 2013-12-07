37e2cdb4becbc739d7b47cb71fc73eee9974924f
Christoffer Werge <thechocobo@gmail.com>
2013-12-07 19:16:04 +0100

    accepttest changes + math afstnit

--- 5 - Software/Simulering/GPSsimu/GPSsimu/Classes/SimulationMath.cs
+++ 5 - Software/Simulering/GPSsimu/GPSsimu/Classes/SimulationMath.cs
@@ -17,8 +17,7 @@ namespace TrackABusSim
                        Math.Pow(Math.Sin(deltaLon / 2), 2) *
                        Math.Cos(deg2rad(double.Parse(lat1))) * Math.Cos(deg2rad(double.Parse(lat2)));
             double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
-            return earthRadius * c * 1000; //meters
-
+            return earthRadius * c * 1000; 
         }
 
         public double BearingDegs(string lat1, string lon1, string lat2, string lon2)
@@ -26,7 +25,6 @@ namespace TrackABusSim
             double deltaLon = deg2rad(double.Parse(lon2) - double.Parse(lon1));
             double lat1Rad = deg2rad(double.Parse(lat1));
             double lat2Rad = deg2rad(double.Parse(lat2));
-
             double x = (Math.Cos(lat1Rad) * Math.Sin(lat2Rad)) -
                         Math.Sin(lat1Rad) * Math.Cos(lat2Rad) * Math.Cos(deltaLon);
             double y = Math.Sin(deltaLon) * Math.Cos(lat2Rad);
@@ -36,7 +34,7 @@ namespace TrackABusSim
 
         public Tuple<double, double> finalGPSPosDeg(string lat, string lon, double bearingDegs, double dist_m)
         {
-            double earthRadius = 6356.752;
+            double earthRadius = 6371;
             double latRad = deg2rad(double.Parse(lat));
             double lonRad = deg2rad(double.Parse(lon));
             double brngRad = deg2rad(bearingDegs);
@@ -44,20 +42,10 @@ namespace TrackABusSim
 
             double finalLatRad = Math.Asin(Math.Sin(latRad) * Math.Cos(angularDistance) +
                                  Math.Cos(latRad) * Math.Sin(angularDistance) * Math.Cos(brngRad));
-            double finalLonRad;
-
-
-            if (Math.Cos(finalLatRad) == 0)
-            {
-                finalLonRad = lonRad;
-            }
-            else
-            {
-
-                double atan1 = Math.Sin(brngRad) * Math.Sin(angularDistance) * Math.Cos(latRad);
-                double atan2 = Math.Cos(angularDistance) - Math.Sin(latRad) * Math.Sin(finalLatRad);
-                finalLonRad = lonRad + Math.Atan2(atan1, atan2);
-            }
+            double atan1 = Math.Sin(brngRad) * Math.Sin(angularDistance) * Math.Cos(latRad);
+            double atan2 = Math.Cos(angularDistance) - Math.Sin(latRad) * Math.Sin(finalLatRad);
+            double finalLonRad = lonRad + Math.Atan2(atan1, atan2);
+            
             double finalLatDeg = rad2deg(finalLatRad);
             double finalLonDeg = ((rad2deg(finalLonRad) + 360) % 360);
             return new Tuple<double, double>(finalLatDeg, finalLonDeg);
@@ -72,6 +60,5 @@ namespace TrackABusSim
         {
             return rad * double.Parse((180 / Math.PI).ToString());
         }
-
     }
 }
