b75c44203d60f94988e9a2eb1ac7fe2d6a6e5ee7
09421 <lasse@micorbis.dk>
2013-12-15 11:43:29 +0100

    Bug fix

--- 5 - Software/AndroidApp/New app/TrackABus/src/dk/TrackABus/BuslistMenuActivity.java
+++ 5 - Software/AndroidApp/New app/TrackABus/src/dk/TrackABus/BuslistMenuActivity.java
@@ -97,7 +97,7 @@ public class BuslistMenuActivity extends ListActivity {
 				if(msg != null){
 					switch(msg.what){
 					case BUSSES_DONE:
-						if(msg.getData().getStringArrayList("1").size() > 0)
+						if(msg.getData().getStringArrayList("1") != null)
 							UpdateBusList(msg.getData().getStringArrayList("1"));
 						else{
 							Toast.makeText(getApplicationContext(), "No bus routes found", Toast.LENGTH_SHORT).show();
